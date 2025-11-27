a10c8364a770740b6fe8a77312ea2fdb4e9edec5
hindustantech <kumarrupees505@gmail.com>
2025-11-27 13:26:25 +0530

    add

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,10 +6,10 @@ import { validate, registerSchema, loginSchema, forgotPasswordSchema, changePass
 
 const router = express.Router();
 
-router.post("/register", validate(registerSchema), register1);
-router.post("/login", validate(loginSchema), login1);
-router.post("/forgot-password", validate(forgotPasswordSchema), forgotPassword1);
-router.post("/change-password", validate(changePasswordSchema), changePassword1);
+router.post("/register", register1);
+router.post("/login", login1);
+router.post("/forgot-password", forgotPassword1);
+router.post("/change-password",  changePassword1);
 router.post("/logout", protect, logout1);
 
 export default router;
