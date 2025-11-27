ea92d7512e54e64b50c779260286607a326f8690
hindustantech <kumarrupees505@gmail.com>
2025-11-27 13:36:33 +0530

    add

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,31 @@
 
 import { registerUser, loginUser, forgotPassword, changePassword, logoutUser } from "../services/authService.js";
 import { logger } from "../config/logger.js";
-
+import { verifyWhatsAppOtp } from "../utils/whatapp.js";
 export const register1 = async (req, res, next) => {
     try {
         const result = await registerUser(req.body);
         res.status(201).json(result);
     } catch (error) {
+        next(error);
+    }
+};
+
+
+export const verifyOtp = async (req, res, next) => {
+    try {
+        const { uid, otp } = req.body;
+        logger.info("verifyOtp called with", { uid });
+
+        const result = await verifyWhatsAppOtp(whatsappNumber, otp);
+        logger.info("verifyOtp result", result);
+        if (!result.success) {
+            return res.status(400).json({ message: "OTP verification failed", details: result });
+        }
+        res.status(200).json({ message: "OTP verified successfully" });
+    }
+    catch (error) {
+        logger.error("Error in verifyOtp:", error);
         next(error);
     }
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,13 @@
 
 import express from "express";
-import { register1, login1, forgotPassword1, changePassword1, logout1 } from "../controllers/authController.js";
+import { register1, login1, forgotPassword1, changePassword1, logout1,verifyOtp } from "../controllers/authController.js";
 import { protect } from "../middleware/authMiddleware.js";
 import { validate, registerSchema, loginSchema, forgotPasswordSchema, changePasswordSchema } from "../utils/validator.js";
 
 const router = express.Router();
 
 router.post("/register", register1);
+router.post("/verifyOtp", verifyOtp);
 router.post("/login", login1);
 router.post("/forgot-password", forgotPassword1);
 router.post("/change-password",  changePassword1);
