fc29de00ab643d9021f895ceb5e5bc6592f9cadb
hindustantech <kumarrupees505@gmail.com>
2025-12-02 11:56:27 +0530

    add

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -4,8 +4,7 @@ import {
     getDashboardStats,
     getDashboardStatsSimple
 } from "../controllers/dashboardController.js";
-import { protect, admin } from "../middleware/authMiddleware.js";
-
+import { protect,admin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 // Get dashboard statistics (Admin only)
